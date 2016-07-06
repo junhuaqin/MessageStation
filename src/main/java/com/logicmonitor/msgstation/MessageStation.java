82d96b46b38875b5e52f9ead3713226f5f80f479
rbtq <robert.qin@logicmonitor.com>
2016-07-06 09:58:55 +0800

    rename to MessagePublisher

--- src/main/java/com/logicmonitor/msgstation/MessageStation.java
+++ src/main/java/com/logicmonitor/msgstation/MessageStation.java
@@ -8,19 +8,19 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by rbtq on 7/5/16.
  */
 public class MessageStation {
-	private Map<String, MessagePublisher> MessageQueues = new ConcurrentHashMap<>();
+	private Map<String, MessagePublisher> MessagePublishers = new ConcurrentHashMap<>();
 
 	public <T> void createTopic(final String topic, MessageStream<T> messageStream) {
-		if (!MessageQueues.containsKey(topic)) {
-			MessageQueues.put(topic, new LinkedMessageQueue<>(messageStream));
+		if (!MessagePublishers.containsKey(topic)) {
+			MessagePublishers.put(topic, new LinkedMessageQueue<>(messageStream));
 		} else {
 			throw new ReadOnlyBufferException();
 		}
 	}
 
 	public <T> void publish(final String topic, T message) throws InterruptedException {
-		if (MessageQueues.containsKey(topic)) {
-			MessageQueues.get(topic).publish(message);
+		if (MessagePublishers.containsKey(topic)) {
+			MessagePublishers.get(topic).publish(message);
 		} else {
 			throw new IllegalArgumentException(String.format("no such topic:%s", topic));
 		}
